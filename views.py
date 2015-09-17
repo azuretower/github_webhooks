fe061d32f1f53367143a00366a5e9ee808b3237e
Ryan <ryan.merrill215@gmail.com>
2015-09-17 11:19:44 -0600

    seperated GET and POST

--- views.py
+++ views.py
@@ -8,13 +8,11 @@ import json
 def update_view(request):
     # checkout this site to figure out better security
     # http://eli.thegreenplace.net/2014/07/09/payload-server-in-python-3-for-github-webhooks
-    if request.method == 'POST' or 'GET':
+    if request.method == 'POST':
         context = {}
 
-        branch = request.POST.get('ref')
         payload = request.body
         json_data = json.loads(payload)
-
         context['branch'] = json_data['ref']
 
         if 'master' in json_data['ref']:
@@ -33,5 +31,26 @@ def update_view(request):
                 context['restart_error'] = restart_error
             except:
                 pass
+
+        return render(request, 'response.html', context)
+
+
+    if request.method == 'GET':
+        context = {}
+
+        pull_response, pull_error = update.pull()
+        context['pull'] = pull_response
+        context['pull_error'] = pull_error
+
+        collect_response, collect_error = update.collect()
+        context['collect'] = collect_response
+        context['collect_error'] = collect_error
+
+        try:
+            restart_response, restart_error = update.restart()
+            context['restart'] = restart_response
+            context['restart_error'] = restart_error
+        except:
+            pass
 
         return render(request, 'response.html', context)
